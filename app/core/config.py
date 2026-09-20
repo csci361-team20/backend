8be1dcd597ef6508a4333f5248b15f6584a02778
pike65 <pike00065@gmail.com>
2026-09-20 22:09:31 +0500

    fix(ci): fix github workflow regarding .env

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    model_config = SettingsConfigDict(
+        env_file=(".env", ".env.example"),
+    )
 
     DATABASE_URL: str
     ORIGINS: list[str]
