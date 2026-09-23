091db99dfd601322621f1aac582514b53c996810
daryn <pike00065@gmail.com>
2026-09-23 18:35:01 +0500

    feat(users): add routes to user module (#20)

--- app/modules/users/router.py
+++ app/modules/users/router.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from fastapi import APIRouter
 
 from app.api.deps import DatabaseSession
@@ -12,6 +14,21 @@ async def get_all_users(db: DatabaseSession):
     return await user_service.get_all_users_from_db(db=db)
 
 
+@router.get("/{id}", response_model=UserResponse)
+async def get_user_by_id(id: UUID, db: DatabaseSession):
+    return await user_service.get_user_from_db_by_id(id=id, db=db)
+
+
 @router.post("/", response_model=UserResponse)
 async def create_user(user_in: UserCreate, db: DatabaseSession):
     return await user_service.add_new_user_to_db(user_in=user_in, db=db)
+
+
+@router.patch("/{id}", response_model=UserResponse)
+async def update_user(id: UUID, user_in: UserCreate, db: DatabaseSession):
+    pass
+
+
+@router.delete("/{id}", response_model=UserResponse)
+async def delete_user(id: UUID, db: DatabaseSession):
+    pass
